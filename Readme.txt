6ab03c624a195be0bfa0342dd1e942c106e5240a
matpakke <nattawoot.github@gmail.com>
2023-11-17 14:28:13 +0700

    Update

---
+++ Readme.txt
@@ -0,0 +1,13 @@
+api/menu-items/
+
+Little Lemon Restaurant
+This project was built using React.
+
+About the project
+Implementations:
+HTML
+CSS
+JavaScrip
+React
+API
+React Routes
---
+++ littlelemon/LittleLemonAPI/urls.py
@@ -0,0 +1,11 @@
+# define URL route for index() view
+from django.urls import path
+from . import views
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path("menu-items/", views.MenuItemsView.as_view(), name="menu-items"),
+    path("menu-items/<int:pk>", views.SingleMenuItemView.as_view()),
+    path("message/", views.msg),
+    path("api-token-auth/", obtain_auth_token),
+]
